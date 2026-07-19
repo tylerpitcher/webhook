630dd8fa8ba2e4727ca402978eeb1be381dedece
Tyler Pitcher <20trlp@queensu.ca>
2026-07-18 20:05:01 -0400

    refactor: GitHubSignatureExtractor.java

--- src/main/java/com/tylerpitcher/webhook/github/GitHubSignatureExtractor.java
+++ src/main/java/com/tylerpitcher/webhook/github/GitHubSignatureExtractor.java
@@ -4,15 +4,15 @@ import java.util.HexFormat;
 
 final class GitHubSignatureExtractor {
 
-    private static final String PREFIX = "sha256=";
+    private static final String SHA256_PREFIX = "sha256=";
 
-    private static final int PREFIX_LENGTH = PREFIX.length();
+    private static final int SHA256_PREFIX_LENGTH = SHA256_PREFIX.length();
 
     public static byte[] extract(String header) {
-        if (!header.startsWith(PREFIX)) {
+        if (!header.startsWith(SHA256_PREFIX)) {
             throw new InvalidGitHubSignatureException("Unsupported signature");
         }
 
-        return HexFormat.of().parseHex(header.substring(PREFIX_LENGTH));
+        return HexFormat.of().parseHex(header.substring(SHA256_PREFIX_LENGTH));
     }
 }
